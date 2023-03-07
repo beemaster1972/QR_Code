083cf25164d9340841858ebc3a3a8b88d83f5cdb
beemaster1972 <ugor.finno@yandex.ru>
2023-03-07 09:48:03 +0500

    06.03.2023

--- qregais.py
+++ qregais.py
@@ -6,9 +6,13 @@ with open('egais_km.txt', mode='r', encoding='UTF8') as f:
     src_km = f.readlines()
     egais_km = [s.strip().split('\t') for s in src_km]
 
-egais_km = [km[0] for km in egais_km if re.match(r'^\d{14}', km[0])]
+egais_km = [km for km in egais_km if re.match(r'^\d{14}', km[0])]
 print(len(egais_km))
-
+for ind, km in enumerate(egais_km):
+    if len(km) == 2:
+        km.append(0)
+egais_km = [km for km in egais_km if km[2]]
+print(len(egais_km))
 page_num = 1
 for ind, km in enumerate(egais_km):
     if not os.path.exists('QR_egais/page'+str(page_num)):
@@ -16,7 +20,8 @@ for ind, km in enumerate(egais_km):
     print('Генерирую -->', ind, km)
     img = qr.make(km)
     img.save('QR_egais/page'+str(page_num)+'/'+str(ind)+'.png')
-    if not ((ind+1) % 250):
+    #img.drawrect()
+    if not ((ind+1) % 100):
         page_num += 1
         #break
 print('Всего страниц', page_num)
